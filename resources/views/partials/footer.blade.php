6d2e009ddb82705a19f6373e5cafbe89b2cd6fdf
DenisConsta <denisconstantin338@gmail.com>
2022-12-22 16:46:45 +0100

    [add] stampa dinamica da controller

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,7 +6,8 @@
 
                 <!-- ? links -->
                 <div class="col links-col d-flex justify-content-between">
-                    {{-- <nav v-for="(nav, index) in links.navs " :key="index" class="py-5 px-2">
+                    {{-- <nav v-for="(nav, index) in links.navs "
+                        class="py-5 px-2">
                         <h3>{{ nav . listName }}</h3>
                         <ul>
                             <li v-for="(link, i) in nav.items" :key="i">
@@ -14,6 +15,16 @@
                             </li>
                         </ul>
                     </nav> --}}
+                    <nav>
+                        <ul>
+                            @foreach ($menu as $item)
+                                <li>
+                                    <a href="#"> {{$item}} </a>
+                                </li>
+                            @endforeach
+
+                        </ul>
+                    </nav>
                 </div>
 
                 <!-- ? Logo -->
@@ -29,7 +40,7 @@
             <nav class="d-flex ">
                 <h3 class="d-none d-md-block">Follow us</h3>
                 <ul class="d-flex m-0 gap-3">
-{{--                     <li v-for="(icon, i) in links.socials" :key="i">
+                    {{--                     <li v-for="(icon, i) in links.socials" :key="i">
                         <img :src="getPathImage(icon)" :alt="icon">
                     </li> --}}
                 </ul>
